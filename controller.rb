4ef53635b774e0652ce0716d2790a64f158e227d
Euan <thecableton@gmail.com>
2018-07-17 16:54:21 +0100

    Pawel shenanigans

--- controller.rb
+++ controller.rb
@@ -2,15 +2,22 @@ require 'sinatra'
 require 'sinatra/contrib/all' if development?
 require_relative './models/game'
 
+get '/welcome' do
+  erb :welcome
+end
+
 get '/:player1_input/:player2_input' do
   @result =  Game.get_winner( params[:player1_input], params[:player2_input])
   erb :result
 end
 #
-# get '/paper/rock' do
-#   return 'Paper Wins'
-# end
-#
+get '/random/:player2_input' do
+  computer = ["rock", "paper", "scissors"].shuffle.first
+  p computer
+  @result =  Game.get_winner( computer, params[:player2_input])
+  erb :result
+end
+
 # get '/scissors/paper' do
 #   return 'Scissors Wins'
 # end
